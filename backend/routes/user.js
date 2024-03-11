7592459561edad1e00384875879b7b124f8ab057
etmohan7@gmail.com <etmohan7@gmail.com>
2024-03-11 11:41:02 +0530

    account.js added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -108,9 +108,30 @@ router.put("/", authMiddleware, async (req, res) => {
         message: "Updated successfully"
     })
 })
+//bulk 
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
 
-router.get("/bulk", authMiddleware,(req,res)=>{
-    
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
 })
 
 
